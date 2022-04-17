c037b717b2d349468b5c3e06fd877db96e51b628
hungtq <hungtq.ttbn@gmail.com>
2022-04-17 14:09:49 +0700

    Fix bug

--- src/main/java/com/tcdt/qlnvkho/repository/khotang/KtNganLoRepository.java
+++ src/main/java/com/tcdt/qlnvkho/repository/khotang/KtNganLoRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface KtNganLoRepository extends CrudRepository<KtNganLo, Long> {
-    final String qr = "SELECT * FROM KT_NGAN_LO WHERE (:ma is null or lower(MA_NGAN_LO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NGAN_LO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or NGANKHO_ID = :id)";
-    final String qrCount = "SELECT count(1) FROM KT_NGAN_LO WHERE (:ma is null or lower(MA_NGAN_LO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NGAN_LO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or NGANKHO_ID = :id)";
+    final String qr = "SELECT * FROM KT_NGAN_LO WHERE (:ma is null or lower(MA_NGANLO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NGANLO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or NGANKHO_ID = :id)";
+    final String qrCount = "SELECT count(1) FROM KT_NGAN_LO WHERE (:ma is null or lower(MA_NGANLO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NGANLO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or NGANKHO_ID = :id)";
 
     @Query(value = qr, countQuery = qrCount, nativeQuery = true)
     Page<KtNganLo> selectParams(@Param("ma") String ma, @Param("ten") String ten, @Param("id") Long id, Pageable pageable);

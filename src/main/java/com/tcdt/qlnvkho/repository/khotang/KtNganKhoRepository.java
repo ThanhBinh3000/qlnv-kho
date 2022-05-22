6edd75e255a56a987624e2a2979a262528a1cc48
hungtq <hungtq.ttbn@gmail.com>
2022-05-22 22:21:56 +0700

    Fix api search ngan-kho

--- src/main/java/com/tcdt/qlnvkho/repository/khotang/KtNganKhoRepository.java
+++ src/main/java/com/tcdt/qlnvkho/repository/khotang/KtNganKhoRepository.java
@@ -1,17 +1,10 @@
 package com.tcdt.qlnvkho.repository.khotang;
 
-import com.tcdt.qlnvkho.table.khotang.KtDiemKho;
 import com.tcdt.qlnvkho.table.khotang.KtNganKho;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface KtNganKhoRepository extends CrudRepository<KtNganKho, Long> {
-    final String qr = "SELECT * FROM KT_NGAN_KHO WHERE (:ma is null or lower(MA_NGAN_KHO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NGAN_KHO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or NHAKHO_ID = :id)";
-    final String qrCount = "SELECT count(1) FROM KT_NGAN_KHO WHERE (:ma is null or lower(MA_NGAN_KHO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NGAN_KHO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or NHAKHO_ID = :id)";
+@Repository
+public interface KtNganKhoRepository extends CrudRepository<KtNganKho, Long>, KtNganKhoRepositoryCustom {
 
-    @Query(value = qr, countQuery = qrCount, nativeQuery = true)
-    Page<KtNganKho> selectParams(@Param("ma") String ma, @Param("ten") String ten, @Param("id") Long id, Pageable pageable);
 }

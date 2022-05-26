c5a166d744b13f71d70ba67806e861072a98ab16
phananhtk <phananh1304@gmail.com>
2022-05-26 13:58:09 +0700

    fix nha kho

--- src/main/java/com/tcdt/qlnvkho/repository/khotang/KtNhaKhoRepository.java
+++ src/main/java/com/tcdt/qlnvkho/repository/khotang/KtNhaKhoRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface KtNhaKhoRepository extends CrudRepository<KtNhaKho, Long> {
-    final String qr = "SELECT * FROM KT_NHA_KHO WHERE (:ma is null or lower(MA_NHA_KHO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NHA_KHO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or DIEMKHO_ID = :id)";
-    final String qrCount = "SELECT count(1) FROM KT_NHA_KHO WHERE (:ma is null or lower(MA_NHA_KHO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NHA_KHO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or DIEMKHO_ID = :id)";
+    final String qr = "SELECT * FROM KT_NHA_KHO WHERE (:ma is null or lower(MA_NHAKHO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NHAKHO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or DIEMKHO_ID = :id)";
+    final String qrCount = "SELECT count(1) FROM KT_NHA_KHO WHERE (:ma is null or lower(MA_NHAKHO) like lower(concat(concat('%', :ma),'%'))) AND (:ten is null or lower(TEN_NHAKHO) like lower(concat(concat('%', :ten),'%'))) and (:id is null or DIEMKHO_ID = :id)";
 
     @Query(value = qr, countQuery = qrCount, nativeQuery = true)
     Page<KtNhaKho> selectParams(@Param("ma") String ma, @Param("ten") String ten, @Param("id") Long id, Pageable pageable);
